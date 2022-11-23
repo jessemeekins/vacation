a94d24958a99b2bbb3182900ebbf7fb6afbd6e37
jessemeekins <87320459+jessemeekins@users.noreply.github.com>
2022-11-23 15:34:40 -0600

    Add files via upload

--- line_generator.py
+++ line_generator.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from vacation_functions import VacationFunctions as vf
+from vacation_functions import *
 import pandas as pd
 from deta import Deta
 
@@ -8,156 +8,154 @@ vacation_lines = deta.Base('VACATION_LINES')
 
 file = pd.read_csv('assets/BshiftbiddersExport.csv')
 
-class LineGenerator:
 
-    def manager_generate_lines(YEAR, SHIFT, DIVISION):
+
+def manager_generate_lines(YEAR, SHIFT, DIVISION, file):
+    
+    lines = get_all_lines_filtered(YEAR, SHIFT, DIVISION)
+
+    current, previous = current_previous_bidders(file, SHIFT, DIVISION)
+    current_index = get_vacation_line_counter(SHIFT, DIVISION)
+
+    for i in range (1,42):
+
+        try:
+            key = 'key'
+            value = f'{YEAR}{SHIFT}{DIVISION}{i}three'
+            line = next(filter(lambda d: d.get(key) == value, lines), None)
+            line_number = line["LINE_NUMBER"]
+            quantity = line["QUANTITIY"]
+            number_of_days = line["NUMBER_OF_DAYS"]
+
+            st.subheader(f"[Line {i}](#line-{i})")
+
+            col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
+            col1.subheader('3 Days ')
+            col2.metric('Num of Picks Left', quantity)
+            if quantity == 0:
+                col3.warning('SOLD OUT!')
+                col4.button('N/A', key=[f'{SHIFT}_{DIVISION}_line{i}_three.1'], disabled=True)
+                col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_three.2'], on_click=update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
+            else:
+                col3.info('OPEN') # create_update_vacation_pick(year, shift, division, line_number, number_of_days, name)
+                col4.button('Bid', key=[f'{SHIFT}_{DIVISION}_line{i}_three.3'], on_click=create_update_vacation_pick, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity-1, current_index+1, current.values.any()])
+                col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_three.4'],on_click=update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
+        except TypeError:
+            pass
+
+        try:
+            key = 'key'
+            value = f'{YEAR}{SHIFT}{DIVISION}{i}four'
+            line = next(filter(lambda d: d.get(key) == value, lines), None)
+            line_number = line["LINE_NUMBER"]
+            quantity = line["QUANTITIY"]
+            number_of_days = line["NUMBER_OF_DAYS"]
+
+            col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
+            col1.subheader('4 Days ')
+            col2.metric('Num of Picks Left', quantity)
+            if quantity == 0:
+                col3.warning('SOLD OUT!')
+                col4.button('N/A', key=[f'{SHIFT}_{DIVISION}_line{i}_four.1'], disabled=True)
+                col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_four.2'], on_click=update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
+            else:
+                col3.info('OPEN') # create_update_vacation_pick(year, shift, division, line_number, number_of_days, name)
+                col4.button('Bid', key=[f'{SHIFT}_{DIVISION}_line{i}_four.3'], on_click=create_update_vacation_pick, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity-1, current_index+1, current.values.any()])
+                col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_four.4'],on_click=update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
         
-        lines = vacation_lines.fetch()
-        lines = lines.items
-  
-        current, previous = vf.current_previous_bidders(file, SHIFT, DIVISION)
+        except TypeError:
+            pass
 
-        current_index = vf.get_vacation_line_counter(SHIFT, DIVISION)
-        for i in range (1,42):
+        try:
+            key = 'key'
+            value = f'{YEAR}{SHIFT}{DIVISION}{i}five'
+            line = next(filter(lambda d: d.get(key) == value, lines), None)
+            line_number = line["LINE_NUMBER"]
+            quantity = line["QUANTITIY"]
+            number_of_days = line["NUMBER_OF_DAYS"]
 
-            try:
-                key = 'key'
-                value = f'{YEAR}{SHIFT}{DIVISION}{i}three'
-                line = next(filter(lambda d: d.get(key) == value, lines), None)
-                line_number = line["LINE_NUMBER"]
-                quantity = line["QUANTITIY"]
-                number_of_days = line["NUMBER_OF_DAYS"]
+            col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
+            col1.subheader('5 Days ')
+            col2.metric('Num of Picks Left', quantity)
+            if quantity == 0:
+                col3.warning('SOLD OUT!')
+                col4.button('N/A', key=[f'{SHIFT}_{DIVISION}_line{i}_five.1'], disabled=True)
+                col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_five.2'], on_click=update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
+            else:
+                col3.info('OPEN')
+                col4.button('Bid', key=[f'{SHIFT}_{DIVISION}_line{i}_five.3'], on_click=create_update_vacation_pick, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity-1, current_index+1, current.values.any()])
+                col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_five.4'], on_click=update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
 
-                st.subheader(f"[Line {i}](#line-{i})")
+        except:
+            pass
+          
+def user_generate_lines(YEAR, SHIFT, DIVISION):
 
-                col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
-                col1.subheader('3 Days ')
-                col2.metric('Num of Picks Left', quantity)
-                if quantity == 0:
-                    col3.warning('SOLD OUT!')
-                    col4.button('N/A', key=[f'{SHIFT}_{DIVISION}_line{i}_three.1'], disabled=True)
-                    col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_three.2'], on_click=vf.update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
-                else:
-                    col3.info('OPEN') # create_update_vacation_pick(year, shift, division, line_number, number_of_days, name)
-                    col4.button('Bid', key=[f'{SHIFT}_{DIVISION}_line{i}_three.3'], on_click=vf.create_update_vacation_pick, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity-1, current_index+1, current.values.any()])
-                    col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_three.4'],on_click=vf.update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
-            except TypeError:
-                pass
+    lines = get_all_lines_filtered(YEAR, SHIFT, DIVISION)
 
-            try:
-                key = 'key'
-                value = f'2023B2{i}four'
-                line = next(filter(lambda d: d.get(key) == value, lines), None)
-                line_number = line["LINE_NUMBER"]
-                quantity = line["QUANTITIY"]
-                number_of_days = line["NUMBER_OF_DAYS"]
+    for i in range(1,42):
+        try:
+            key = 'key'
+            value = f'{YEAR}{SHIFT}{DIVISION}{i}three'
+            line = next(filter(lambda d: d.get(key) == value, lines), None)
+            line_number = line["LINE_NUMBER"]
+            quantity = line["QUANTITIY"]
+            number_of_days = line["NUMBER_OF_DAYS"]
 
-                col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
-                col1.subheader('4 Days ')
-                col2.metric('Num of Picks Left', quantity)
-                if quantity == 0:
-                    col3.warning('SOLD OUT!')
-                    col4.button('N/A', key=[f'{SHIFT}_{DIVISION}_line{i}_four.1'], disabled=True)
-                    col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_four.2'], on_click=vf.update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
-                else:
-                    col3.info('OPEN') # create_update_vacation_pick(year, shift, division, line_number, number_of_days, name)
-                    col4.button('Bid', key=[f'{SHIFT}_{DIVISION}_line{i}_four.3'], on_click=vf.create_update_vacation_pick, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity-1, current_index+1, current.values.any()])
-                    col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_four.4'],on_click=vf.update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
+            st.subheader(f"[Line {i}](#line-{i})")
+
+            col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
+            col1.subheader('3 Days')
+            col2.metric('Num of Picks Left', quantity)
+            if quantity == 0:
+                col3.warning('SOLD OUT!')
+            elif quantity == 1:
+                col3.warning('ALMOST SOLD OUT!')    
+            else:
+                col3.info('OPEN')
+        except TypeError:
+            pass
+
+        try:
+            key = 'key'
+            value = f'{YEAR}{SHIFT}{DIVISION}{i}four'
+            line = next(filter(lambda d: d.get(key) == value, lines), None)
+            line_number = line["LINE_NUMBER"]
+            quantity = line["QUANTITIY"]
+            number_of_days = line["NUMBER_OF_DAYS"]
+
+            col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
+            col1.subheader('4 Days')
+            col2.metric('Num of Picks Left', quantity)
+            if quantity == 0:
+                col3.warning('SOLD OUT!')
+            elif quantity == 1:
+                col3.warning('ALMOST SOLD OUT!')    
+            else:
+                col3.info('OPEN')
+        except TypeError:
+            pass  
+
+        try:
+            key = 'key'
+            value = f'{YEAR}{SHIFT}{DIVISION}{i}five'
+            line = next(filter(lambda d: d.get(key) == value, lines), None)
+            line_number = line["LINE_NUMBER"]
+            quantity = line["QUANTITIY"]
+            number_of_days = line["NUMBER_OF_DAYS"]
+
+            col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
+            col1.subheader('5 Days')
+            col2.metric('Num of Picks Left', quantity)
+            if quantity == 0:
+                col3.warning('SOLD OUT!')
+            elif quantity == 1:
+                col3.warning('ALMOST SOLD OUT!')    
+            else:
+                col3.info('OPEN')
+        except TypeError:
+            pass    
             
-            except TypeError:
-                pass
-
-            try:
-                key = 'key'
-                value = f'2023B2{i}five'
-                line = next(filter(lambda d: d.get(key) == value, lines), None)
-                line_number = line["LINE_NUMBER"]
-                quantity = line["QUANTITIY"]
-                number_of_days = line["NUMBER_OF_DAYS"]
-
-                col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
-                col1.subheader('5 Days ')
-                col2.metric('Num of Picks Left', quantity)
-                if quantity == 0:
-                    col3.warning('SOLD OUT!')
-                    col4.button('N/A', key=[f'{SHIFT}_{DIVISION}_line{i}_five.1'], disabled=True)
-                    col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_five.2'], on_click=vf.update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
-                else:
-                    col3.info('OPEN')
-                    col4.button('Bid', key=[f'{SHIFT}_{DIVISION}_line{i}_five.3'], on_click=vf.create_update_vacation_pick, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity-1, current_index+1, current.values.any()])
-                    col5.button('Remove', key=[f'{SHIFT}_{DIVISION}_line{i}_five.4'], on_click=vf.update_vacation_line, args=[YEAR, SHIFT, DIVISION, i, number_of_days, quantity+1, current_index-1])
-
-            except:
-                pass
-               
-    def user_generate_lines(YEAR, SHIFT, DIVISION):
-        
-        lines = vacation_lines.fetch()
-        lines = lines.items
-
-        for i in range(1,42):
-            try:
-                key = 'key'
-                value = f'2023B2{i}three'
-                line = next(filter(lambda d: d.get(key) == value, lines), None)
-                line_number = line["LINE_NUMBER"]
-                quantity = line["QUANTITIY"]
-                number_of_days = line["NUMBER_OF_DAYS"]
-
-                st.subheader(f"[Line {i}](#line-{i})")
-
-                col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
-                col1.subheader('3 Days')
-                col2.metric('Num of Picks Left', quantity)
-                if quantity == 0:
-                    col3.warning('SOLD OUT!')
-                elif quantity == 1:
-                    col3.warning('ALMOST SOLD OUT!')    
-                else:
-                    col3.info('OPEN')
-            except TypeError:
-                pass
-
-            try:
-                key = 'key'
-                value = f'2023B2{i}four'
-                line = next(filter(lambda d: d.get(key) == value, lines), None)
-                line_number = line["LINE_NUMBER"]
-                quantity = line["QUANTITIY"]
-                number_of_days = line["NUMBER_OF_DAYS"]
-
-                col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
-                col1.subheader('4 Days')
-                col2.metric('Num of Picks Left', quantity)
-                if quantity == 0:
-                    col3.warning('SOLD OUT!')
-                elif quantity == 1:
-                    col3.warning('ALMOST SOLD OUT!')    
-                else:
-                    col3.info('OPEN')
-            except TypeError:
-                pass  
-
-            try:
-                key = 'key'
-                value = f'2023B2{i}five'
-                line = next(filter(lambda d: d.get(key) == value, lines), None)
-                line_number = line["LINE_NUMBER"]
-                quantity = line["QUANTITIY"]
-                number_of_days = line["NUMBER_OF_DAYS"]
-
-                col1, col2, col3, col4, col5= st.columns([1,1,1,0.5,1])
-                col1.subheader('5 Days')
-                col2.metric('Num of Picks Left', quantity)
-                if quantity == 0:
-                    col3.warning('SOLD OUT!')
-                elif quantity == 1:
-                    col3.warning('ALMOST SOLD OUT!')    
-                else:
-                    col3.info('OPEN')
-            except TypeError:
-                pass    
-                
 
 
 
--- vacation_functions.py
+++ vacation_functions.py
@@ -11,107 +11,110 @@ vacation_lines = deta.Base('VACATION_LINES')
 vacation_picks = deta.Base('VACATION_PICKS')
 
 @st.cache
-def upload_data(file):
+def upload_data():
+    file = pd.read_csv('assets/BshiftbiddersExport.csv')
     df = pd.DataFrame(file)
     return df
 
-class VacationFunctions:
-    ### INDEX COUNTER CRUD ###
-    def put_vaction_line_counter(shift, division, count):
-        counters.put({"DIVISION": division, "SHIFT": shift, "COUNT": count}, key=f'{shift}{division}')
-        st.success(f' Line count updated to {count}')
+### INDEX COUNTER CRUD ###
+def put_vaction_line_counter(shift, division, count):
+    counters.put({"DIVISION": division, "SHIFT": shift, "COUNT": count}, key=f'{shift}{division}')
+    st.success(f' Line count updated to {count}')
 
-    def get_vacation_line_counter(shift, division):
-        counter = counters.get(f'{shift}{division}') 
-        return counter["COUNT"]
+def get_vacation_line_counter(shift, division):
+    counter = counters.get(f'{shift}{division}') 
+    return counter["COUNT"]
+
+def update_vacation_line_counter(shift, division, count):
+    counters.update({"COUNT": count}, key=f'{shift}{division}')
+    st.session_state.index_counter = count
+
+
+def delete_vacation_line_counter(shift, division):
+    counters.delete(key=f'{shift}{division}')
+    st.success(f'{shift} Shift Division {division} counter was deleted.')
+
+### Employee CRUD ###
+def put_update_employee(year, shift, division, seniority_date, name):
+    employee.put({"YEAR": year, "SHIFT": shift, "DIVISION": division, "SENIORITY_DATE": seniority_date, "NAME": name, "PICKS": []}, key=f'{year}{shift}{division}{name}')
+    st.success(f'{name} was added/updated to databse.')
+
+def get_employee(year, shift, division, name):
+    return employee.get(f'{year}{shift}{division}{name}')
+
+def update_employee(year, shift, division, name, **kwargs):
+    updates = {}        
+    for kwarg in kwargs:
+        key, value = kwarg
+        updates.update(key, value)
+    employee.update(updates, f'{year}{shift}{division}{name}')    
+
+def delete_employee(year, shift, division, name):
+    employee.delete({f'{year}{shift}{division}{name}'})
+    st.success(f'{name} was deleted from database.')
+
+def read_employee(**kwargs):
+    employee_list = []
+    for kwarg in kwargs:
+        e = employee.fetch({"NAME": kwarg})
+        employee_list.append(e.items)
+    return employee_list
     
-    def update_vacation_line_counter(shift, division, count):
-        counters.update({"COUNT": count}, key=f'{shift}{division}')
-        st.session_state.index_counter = count
+### VACATION LINES CRUD ###
+@st.cache
+def get_all_lines_filtered(year, shift, division):
+    all_vacation_lines = vacation_lines.fetch([{"YEAR?gt": year}, {"SHIFT": shift}, {"DIVISION": division}])
+    return all_vacation_lines.items
+
+def create_vacation_line(year, shift, division, line_number, number_of_days, quantity):
+    vacation_lines.put({"YEAR": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days, "QUANTITIY": quantity,}, f'{year}{shift}{division}{line_number}{number_of_days}')
+    st.success(f'{shift} {division} {line_number} {number_of_days} added.')
+
+def update_vacation_line(year, shift, division, line_number, number_of_days, quantity, index):
+    vacation_lines.update({"QUANTITIY": quantity}, key=f'{year}{shift}{division}{line_number}{number_of_days}')
+    counters.update({"COUNT": index}, f'{shift}{division}')
+
+
+def delete_vacation_line(year, shift, division, line_number, number_of_days):
+    vacation_lines.delete({f'{year}{shift}{division}{line_number}{number_of_days}'})
     
 
-    def delete_vacation_line_counter(shift, division):
-        counters.delete(key=f'{shift}{division}')
-        st.success(f'{shift} Shift Division {division} counter was deleted.')
+def get_vacation_line(year, shift, division, line_number, number_of_days):
+    vacation_line = vacation_lines.get(f'{year}{shift}{division}{line_number}{number_of_days}')
+    return vacation_line
 
-    ### Employee CRUD ###
-    def put_update_employee(year, shift, division, seniority_date, name):
-        employee.put({"YEAR": year, "SHIFT": shift, "DIVISION": division, "SENIORITY_DATE": seniority_date, "NAME": name, "PICKS": []}, key=f'{year}{shift}{division}{name}')
-        st.success(f'{name} was added/updated to databse.')
+### VACATION PICK CRUD ###  
+def create_update_vacation_pick(year, shift, division, line_number, number_of_days, num, index, name):
+    vacation_picks.put({"YEAR": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days, "NAME": name}, f'{year}{shift}{division}{line_number}{number_of_days}{name}')
+    vacation_lines.update({"QUANTITIY": num} ,f'{year}{shift}{division}{line_number}{number_of_days}')
+    counters.update({'COUNT': index}, f'{shift}{division}')
+    st.session_state.last_pick = f'Line {line_number} {number_of_days} day.'
+    
+def add_person_too_line(year, shift, division, line_number, number_of_days, name):
+    vacation_picks.put({"YEAR": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days, "NAME": name}, f'{year}{shift}{division}{line_number}{number_of_days}{name}')
+    st.success(f'{name} was added from Line {line_number} {number_of_days}')
 
-    def get_employee(year, shift, division, name):
-        return employee.get(f'{year}{shift}{division}{name}')
+def delete_vacation_pick(year, shift, division, line_number, number_of_days, name):
+    vacation_picks.delete(f'{year}{shift}{division}{line_number}{number_of_days}{name}')
+    st.success(f'{name} was deleted from Line {line_number} {number_of_days}')
 
-    def update_employee(year, shift, division, name, **kwargs):
-        updates = {}        
-        for kwarg in kwargs:
-            key, value = kwarg
-            updates.update(key, value)
-        employee.update(updates, f'{year}{shift}{division}{name}')    
+def read_vacation_pick(year, shift, division, line_number, number_of_days):
+    vacation_picks = vacation_picks.fetch({"YEAR?gt": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days})
+    return vacation_picks
 
-    def delete_employee(year, shift, division, name):
-        employee.delete({f'{year}{shift}{division}{name}'})
-        st.success(f'{name} was deleted from database.')
+def current_previous_bidders( df, shift, division):
+    index = get_vacation_line_counter(shift, division)
+    current_bidder = df[['Ordinal','RscMasterNameCh']].iloc[[index]]
+    previous_bidder = df[['Ordinal','RscMasterNameCh']].iloc[[index-1]]
+    return current_bidder, previous_bidder
 
-    def read_employee(**kwargs):
-        employee_list = []
-        for kwarg in kwargs:
-            e = employee.fetch({"NAME": kwarg})
-            employee_list.append(e.items)
-        return employee_list
-        
-    ### VACATION LINES CRUD ###
-    def get_all_lines_filtered(year, shift, division):
-        all_vacation_lines = vacation_lines.fetch([{"YEAR?gt": year}, {"SHIFT": shift}, {"DIVISION": division}])
-        return all_vacation_lines.items
+@st.cache
+def get_people_on_line(year, shift, division, line_number, number_of_days):
+    names = vacation_picks.fetch({"YEAR": year,"SHIFT": shift, "DIVISION": division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days})
+    return names
 
-    def create_vacation_line(year, shift, division, line_number, number_of_days, quantity):
-        vacation_lines.put({"YEAR": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days, "QUANTITIY": quantity,}, f'{year}{shift}{division}{line_number}{number_of_days}')
-        st.success(f'{shift} {division} {line_number} {number_of_days} added.')
-
-    def update_vacation_line(year, shift, division, line_number, number_of_days, quantity, index):
-        vacation_lines.update({"QUANTITIY": quantity}, key=f'{year}{shift}{division}{line_number}{number_of_days}')
-        counters.update({"COUNT": index}, f'{shift}{division}')
-
-
-    def delete_vacation_line(shift, division, line_number, number_of_days):
-        vacation_lines.delete({f'{shift}{division}{line_number}{number_of_days}'})
-        st.success(f'{shift}{division}{line_number}{number_of_days} deleted.')
-
-    def get_vacation_line(year, shift, division, line_number, number_of_days):
-        vacation_line = vacation_lines.get(f'{year}{shift}{division}{line_number}{number_of_days}')
-        return vacation_line
-
-    ### VACATION PICK CRUD ###  
-    def create_update_vacation_pick(year, shift, division, line_number, number_of_days, num, index, name):
-        vacation_picks.put({"YEAR": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days, "NAME": name}, f'{year}{shift}{division}{line_number}{number_of_days}{name}')
-        vacation_lines.update({"QUANTITIY": num} ,f'{year}{shift}{division}{line_number}{number_of_days}')
-        counters.update({'COUNT': index}, f'{shift}{division}')
-        st.session_state.last_pick = f'Line {line_number} {number_of_days} day.'
-        
-    def add_person_too_line(year, shift, division, line_number, number_of_days, name):
-        vacation_picks.put({"YEAR": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days, "NAME": name}, f'{year}{shift}{division}{line_number}{number_of_days}{name}')
-        st.success(f'{name} was added from Line {line_number} {number_of_days}')
-
-    def delete_vacation_pick(year, shift, division, line_number, number_of_days, name):
-        vacation_picks.delete(f'{year}{shift}{division}{line_number}{number_of_days}{name}')
-        st.success(f'{name} was deleted from Line {line_number} {number_of_days}')
-
-    def read_vacation_pick(year, shift, division, line_number, number_of_days):
-        vacation_picks = vacation_picks.fetch({"YEAR?gt": year, "SHIFT": shift, 'DIVISION': division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days})
-        return vacation_picks
-
-    def current_previous_bidders( df, shift, division):
-        index = VacationFunctions.get_vacation_line_counter(shift, division)
-        current_bidder = df[['Ordinal','RscMasterNameCh']].iloc[[index]]
-        previous_bidder = df[['Ordinal','RscMasterNameCh']].iloc[[index-1]]
-        return current_bidder, previous_bidder
-
-    def get_people_on_line(year, shift, division, line_number, number_of_days):
-        names = vacation_picks.fetch({"YEAR": year,"SHIFT": shift, "DIVISION": division, "LINE_NUMBER": line_number, "NUMBER_OF_DAYS": number_of_days})
-        return names
-
-    def get_lines():
-        lines = vacation_lines.fetch()
-        return lines.items
+@st.cache
+def get_lines():
+    lines = vacation_lines.fetch()
+    return lines.items
 
